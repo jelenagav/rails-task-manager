41a9a4ceb363fe1cc2a10d0b37df3f5f6df5db1c
Jelena Gavrilovic <gavrilovicjelena@outlook.com>
2019-02-19 17:10:49 +0100

    partially

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,36 @@
+class TasksController < ApplicationController
+  def index
+   @tasks = Task.all
+  end
+
+  def show
+  # @id = params[:id]
+   @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+  @task = Task.create(task_params)
+  redirect_to task_path(@task)
+  end
+
+  def edit
+  @task = Task.find(params[:id])
+  end
+
+  def update
+    id = params[:id]
+    @task = Task.find(id)
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+end
